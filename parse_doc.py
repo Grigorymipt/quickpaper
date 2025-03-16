6bd44e21ef1ff12d7d8c9405d33fdfcab5a4f2c8
Grigorymipt <koveshnikov.gv@phystech.edu>
2025-03-16 19:42:44 +0300

    add status bar

--- parse_doc.py
+++ parse_doc.py
@@ -5,7 +5,10 @@ import numpy as np
 
 def read_docx(file_path, transformation):
     doc = Document(file_path)
+    count = 0
     for paragraph in doc.paragraphs:
         paragraph.text = transformation(paragraph.text)
+        count += 1
+        print(count/len(doc.paragraphs))
     doc.save("output.docx")
 
